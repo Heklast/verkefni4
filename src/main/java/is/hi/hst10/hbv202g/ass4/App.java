8938774272319d5a6a09a8e6a3f804197a7d611e
Heklast <hst10@hi.is>
2023-02-10 09:06:08 +0000

    Initial commit

---
+++ src/main/java/is/hi/hst10/hbv202g/ass4/App.java
@@ -0,0 +1,13 @@
+package is.hi.hst10.hbv202g.ass4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
