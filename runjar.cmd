9446d9ef35cc955fc7f59129a4b45e22bf3e371a
Heklast <hst10@hi.is>
2023-02-16 17:46:21 +0000

    commita allt

---
+++ runjar.cmd
@@ -0,0 +1,1 @@
+java -cp target/dependency/commons-numbers-primes-1.1.jar
--- src/main/java/is/hi/hst10/hbv202g/ass4/App.java
+++ src/main/java/is/hi/hst10/hbv202g/ass4/App.java
@@ -1,13 +1,12 @@
 package is.hi.hst10.hbv202g.ass4;
 
+import org.apache.commons.numbers.primes.Primes;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println(Primes.nextPrime(123456789));
     }
 }
